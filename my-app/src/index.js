b61197b326c36649b6cdb5e22b277a530a9e3db8
Timothy Fawcett <tim.fawcett89@gmail.com>
2022-10-16 13:29:27 +1100

    my first react commit

---
+++ my-app/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+
+function App() {
+  return <body>
+    <main>
+    <div>
+    <img src="" alt="equilibrium number 3429" />
+    <h1>Equilibrium #3429</h1>
+    <h2>Our Equilibrium collection promotes balance and calm.</h2>
+    <h3>0.041 ETH</h3>
+    <h4>3 days left</h4>
+    </div>
+    <img src="" alt="creator" />
+      <h5>Creation of Jules Wyvern</h5>
+    </main>
+    <footer>
+      <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" >Frontend Mentor</a>. 
+    Coded by <a href="https://www.frontendmentor.io/profile/timbosTours">Timothy Fawcett</a>.
+  </div>
+    </footer>
+    </body>
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
